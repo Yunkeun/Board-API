2e331bd713decfc20c0f31275e892af385cec955
Yunkeun <yunx2@naver.com>
2021-08-16 20:28:00 +0900

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+var cors = require("cors");
+var connectDB = require("./config/db");
+
+connectDB();
+
+var corsOptions = {
+  origin: true,
+  credentials: true
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/board", require("./routes/boardRouter"));
+
+app.listen(8080, () => {
+  console.log("listening");
+});
